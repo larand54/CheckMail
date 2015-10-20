08180551d4314730ea41f0d1848f3fbc1c8c7ed0
laran <laran@DESKTOP-01MAENE>
2015-10-20 10:30:51 +0200

    Vet ej vad som ändrats och varför men inget går att kompilera
    Kompilering och körning är disablat av någon anledning!

--- src/com/larand54/mail/CheckingMails.java
+++ src/com/larand54/mail/CheckingMails.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Version 0.900
 package com.larand54.mail;
 
 import java.io.FileInputStream;
